85884a5616e137c3137c2fc52dfa10c3e4deca18
felixs-alt <felixplays25@gmail.com>
2024-10-28 09:52:03 +0000

    f

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const ws = require('ws')
 var request = require('request');
+const crypto = require('crypto');
 const socketio = require('socket.io');
 var cors = require('cors')
 const app = express()
 const wss = new ws.WebSocketServer({ port: 2929,clientTracking: true });
 const port = 3000
 let userCount = 1;
+const secret = '175bfc31f2df4eddbe58df28bcae676b';
+
+// signing the secret with the timestamp from the request
+function sign(secret, timestamp) {
+  const hmac = crypto.createHmac('sha256', secret);
+  hmac.update(timestamp);
+  return hmac.digest('hex');
+}
 
 app.use(cors())
 app.use("/", express.static(__dirname+'/public'));
@@ -17,6 +26,14 @@ app.get('/api/users', function(req,res) {
 
 app.post("/tmrw", (req, res) => {
   console.log(req.body) // Call your action on the request here
+  // tomorrow.io signature header is "t={timestamp},sig={signature}"
+  const signatureHeader = req.headers['X-Signature'].split(',');
+  // extract timestamp
+  const timestamp = signatureHeader[0].split('=')[1];
+  // extract signature
+  const signature = signatureHeader[1].split('=')[1];
+  // getting the expected signature
+  const expectedSignature = sign(secret, timestamp);
   res.status(200).end() // Responding is important
 })
 
